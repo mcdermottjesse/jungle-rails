c1377821c56ca1d2eb7e5297ade5f951819628cf
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-04-01 01:54:15 +0000

    add about controller and about route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
+  resources :about, only: [:index]
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  
 
   resource :cart, only: [:show] do
     post   :add_item
