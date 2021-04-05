a23c0efb25d65e75821279824a4046a421082e56
Jesse McDermott <mcdermott.jesse9@gmail.com>
2021-04-05 19:38:07 +0000

    rspec setup

--- db/schema.rb
+++ db/schema.rb
@@ -58,12 +58,11 @@ ActiveRecord::Schema.define(version: 20210401220306) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.string   "last_name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    
+    t.string   "last_name"
   end
 
   add_foreign_key "line_items", "orders"
